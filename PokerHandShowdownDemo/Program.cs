6ca752e90d1202cb1949e0b222552d976fbdc38f
hicham <hichambagui@gmail.com>
2020-10-25 15:33:33 -0500

    Create demo project

---
+++ PokerHandShowdownDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using PokerHandShowdown;
+
+namespace PokerHandShowdownDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hand hand = new Hand();
+        }
+    }
+}
